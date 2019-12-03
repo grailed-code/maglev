58c9a09397ccada67138e65973396d89060b9e70
Evan Siegel <evan@grailed.com>
2019-12-03 14:02:11 -0500

    Adds Env.getArray

--- src/Env.ts
+++ src/Env.ts
@@ -1,4 +1,7 @@
 require("dotenv").config();
+import { flow } from "fp-ts/lib/function";
+import { map } from "fp-ts/lib/Array";
+import { split, trim } from "./String.Extra";
 
 /**
  * get :: String -> String
@@ -13,3 +16,12 @@ export const get = (key: string): string => {
 
   throw new Error(`${key} must be set in ENV.`);
 };
+
+/**
+ * getArray :: String -> Array String
+ */
+export const getArray = flow(
+  get,
+  split(","),
+  map(trim),
+);
