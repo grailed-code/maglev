79b1084824ad894c2ef7d20a4e6d043ab07ae233
Evan Siegel <evan@grailed.com>
2019-12-11 11:41:27 -0500

    Improves Logger.log with JSON.stringify

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,8 +1,17 @@
+import { flow } from "fp-ts/lib/function";
 import { tap } from "./Function.Extra";
+
+const jsonStringify: <A>(a: A) => string = JSON.stringify;
+const consoleLog: <A>(a: A) => void = console.log.bind(console);
 
 /**
  * log :: A -> A
  *
  * Logs the given value and returns the given value unchanged.
  */
-export const log: <A>(a: A) => A = tap((a) => console.log(a));
+export const log: <A>(a: A) => A = tap(
+  flow(
+    jsonStringify,
+    consoleLog,
+  ),
+);
