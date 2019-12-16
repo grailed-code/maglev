9bac19588717a242f63da3ab16161a6a7d913536
Evan Siegel <evan@grailed.com>
2019-12-16 15:59:55 -0500

    ✨ Adds .getMostRecent to Heroku.Build

--- src/Heroku/Build.ts
+++ src/Heroku/Build.ts
@@ -1,9 +1,12 @@
+import { head, filter } from "fp-ts/lib/Array";
 import { flow, constant, not } from "fp-ts/lib/function";
+import { Option } from "fp-ts/lib/Option";
 import {
   TaskEither,
   map,
   chain,
   fromPredicate,
+  fromOption,
 } from "fp-ts/lib/TaskEither";
 import { any } from "../Array.Extra";
 import { Request, RequestError } from "../Request";
@@ -40,6 +43,7 @@ export interface Build {
 }
 
 export const isPending = ({ status }: Build): boolean => status === "pending";
+export const isFailed = ({ status }: Build): boolean => status === "failed";
 
 interface CreateParams {
   source_blob: SourceBlob;
@@ -73,6 +77,54 @@ export const all = flow(
   (app: string) => API.get<Array<Build>>(`/apps/${app}/builds`),
   map((res) => res.data),
 );
+
+export interface NotFoundError {
+  kind: "Heroku.Build.NotFoundError";
+  message: "No Heroku Build Found";
+  stack?: string;
+  app: string;
+}
+
+/**
+ * getMostRecent :: String -> Task String Build
+ *
+ * Returns a task of the most recent Heroku build from the given app.
+ *
+ * The /apps/:name/builds endpoint can be sorted and filtered by a given range. In this case, we
+ * want to get the most recent build, so we set the order to be descending and limit the request
+ * to maximum of 1 result.
+ *
+ * We will always get back an array of builds, even if we specify a max of 1, so we need to grab
+ * the head of the array of builds.
+ *
+ * Array.head returns an Option of whatever is in the array, in our case a build. In our program,
+ * we only want to consider this operation as successful if we actually have a build at the end,
+ * if our Option of a Build resolves to a None, we can convert that into a failing TaskEither.
+ *
+ * In any case, we are returning a TaskEither of a Build, not a TaskEither of an Option of a Build.
+ */
+export const getMostRecent = (
+  appName: string,
+): TaskEither<RequestError | NotFoundError, Build> =>
+  flow(
+    (appName: string) =>
+      API.get<Array<Build>>(
+        `/apps/${appName}/builds`,
+        "created_at; order=desc, max=1",
+      ),
+    map((res) => res.data),
+    map(filter(not(isFailed))),
+    map(head),
+    chain<RequestError | NotFoundError, Option<Build>, Build>(
+      fromOption(
+        constant({
+          kind: "Heroku.Build.NotFoundError",
+          message: "No Heroku Build Found",
+          app: appName,
+        }),
+      ),
+    ),
+  )(appName);
 
 export interface CreateError {
   kind: "Heroku.Build.CreateError";
