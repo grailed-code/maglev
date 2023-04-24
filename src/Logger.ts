49c5b7b0c74693721410c5206accab73c4a3b187
brianabaker <brianaclairbaker@gmail.com>
2023-04-24 13:06:34 -0400

    stop stringifying logs

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,7 +1,7 @@
 import { flow } from "fp-ts/lib/function";
 import { tap } from "./Function.Extra";
 
-const jsonStringify: <A>(a: A) => string = JSON.stringify;
+// const jsonStringify: <A>(a: A) => string = JSON.stringify;
 const consoleLog: <A>(a: A) => void = console.log.bind(console);
 
 /**
@@ -11,7 +11,6 @@ const consoleLog: <A>(a: A) => void = console.log.bind(console);
  */
 export const log: <A>(a: A) => A = tap(
   flow(
-    jsonStringify,
     consoleLog,
   ),
 );
