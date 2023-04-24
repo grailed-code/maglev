75c4577189ea24364ef1e63df46a907e94aaed7f
brianabaker <brianaclairbaker@gmail.com>
2023-04-24 14:07:24 -0400

    custom stringify for circular refrence

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,7 +1,25 @@
 import { flow } from "fp-ts/lib/function";
 import { tap } from "./Function.Extra";
 
-// const jsonStringify: <A>(a: A) => string = JSON.stringify;
+const jsonStringify = <A>(obj: A): string => {
+  let cache: Array<any>  = [];
+
+  let str = JSON.stringify(obj, (_key, value: any) => {
+    if (typeof value === "object" && value !== null) {
+      if (cache.indexOf(value) !== -1) {
+        // Circular reference found, discard key
+        return;
+      }
+      // Store value in our collection
+      cache.push(value);
+    }
+    return value;
+  });
+
+  cache = []; // reset the cache
+  return str;
+}
+
 const consoleLog: <A>(a: A) => void = console.log.bind(console);
 
 /**
@@ -11,6 +29,7 @@ const consoleLog: <A>(a: A) => void = console.log.bind(console);
  */
 export const log: <A>(a: A) => A = tap(
   flow(
+    jsonStringify,
     consoleLog,
   ),
 );
